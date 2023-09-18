847dcff8ca0b52409323e3f1931b4db635701c16
unknown <mochamadraihansutisna@gmail.com>
2023-09-18 18:31:02 +0700

    1.0

---
+++ JS-WebProgrammingUnpas/latihan dom/script.js
@@ -0,0 +1,31 @@
+function getPilihanComputer() {
+    const comp = Math.random();
+
+    if (comp < 0.34) return "gajah";
+
+    if (comp >= 0.34 && comp < 0.67) return "orang";
+    return "semut";
+}
+
+
+
+function getHasil(comp, player) {
+
+    if (player == comp) return 'SERI!';
+    if (player == 'gajah') return (comp == 'orang') ? 'MENANG!' : 'KALAH!';
+    if (player == 'orang') return (comp == 'gajah') ? 'KALAH!' : 'MENANG!';
+    if (player == 'semut') return (comp == 'orang') ? 'KALAH!' : 'MENANG!';
+}
+
+
+
+const pGajah = document.querySelector(".gajah");
+pGajah.addEventListener("click", function() {
+    const pilihanComputer = getPilihanComputer();
+    const pilihanPlayer = pGajah.className;
+    const hasil = getHasil(pilihanComputer, pilihanPlayer);
+
+    console.log("comp : " + pilihanComputer);
+    console.log("player : " + pilihanPlayer);
+    console.log("hasil : " + hasil);
+})
