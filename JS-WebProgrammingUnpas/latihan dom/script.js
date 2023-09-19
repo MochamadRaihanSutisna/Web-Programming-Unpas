327658f47b1d9aeceb4e27f8aa45035842a03e75
unknown <mochamadraihansutisna@gmail.com>
2023-09-20 01:58:43 +0700

    commit 20.9.23

--- JS-WebProgrammingUnpas/latihan dom/script.js
+++ JS-WebProgrammingUnpas/latihan dom/script.js
@@ -18,14 +18,99 @@ function getHasil(comp, player) {
 }
 
 
+function putar() {
+    const imgComputer = document.querySelector(".img-computer");
+    const gambar = ["gajah", "semut", "orang"];
 
-const pGajah = document.querySelector(".gajah");
-pGajah.addEventListener("click", function() {
-    const pilihanComputer = getPilihanComputer();
-    const pilihanPlayer = pGajah.className;
-    const hasil = getHasil(pilihanComputer, pilihanPlayer);
+    let i = 0;
+    const waktuMulai = new Date().getTime();
+    setInterval(function() {
+        if (new Date().getTime() - waktuMulai > 1000) {
+            clearInterval;
+            return;
+        }
 
-    console.log("comp : " + pilihanComputer);
-    console.log("player : " + pilihanPlayer);
-    console.log("hasil : " + hasil);
+        imgComputer.setAttribute("src", "img/" + gambar[i++] + ".png");
+        if (i == gambar.length) i = 0;
+    }, 100);
+}
+
+
+const pilihan = document.querySelectorAll("li img");
+pilihan.forEach(function(pil) {
+    pil.addEventListener("click", function() {
+        const pilihanComputer = getPilihanComputer();
+        const pilihanPlayer = pil.className;
+        const hasil = getHasil(pilihanComputer, pilihanPlayer);
+
+
+        putar();
+        setTimeout(function() {
+            const imgComputer = document.querySelector(".img-computer");
+            imgComputer.setAttribute("src", "img/" + pilihanComputer + ".png");
+
+            const info = document.querySelector(".info");
+            info.innerHTML = hasil;
+
+        }, 1001)
+
+    })
 })
+
+
+/*
+
+//> Start Events Gajah
+const pGajah = document.querySelector(".gajah");
+
+pGajah.addEventListener("click", function() {
+        const pilihanComputer = getPilihanComputer();
+        const pilihanPlayer = pGajah.className;
+        const hasil = getHasil(pilihanComputer, pilihanPlayer);
+
+        const imgComputer = document.querySelector(".img-computer");
+        imgComputer.setAttribute("src", "img/" + pilihanComputer + ".png");
+
+        const info = document.querySelector(".info");
+        info.innerHTML = hasil;
+
+    })
+    //> Finish Events Gajah
+
+
+//> Start Events Orang
+const pOrang = document.querySelector(".orang");
+
+pOrang.addEventListener("click", function() {
+        const pilihanComputer = getPilihanComputer();
+        const pilihanPlayer = pOrang.className;
+        const hasil = getHasil(pilihanComputer, pilihanPlayer);
+
+        const imgComputer = document.querySelector(".img-computer");
+        imgComputer.setAttribute("src", "img/" + pilihanComputer + ".png");
+
+        const info = document.querySelector(".info");
+        info.innerHTML = hasil;
+
+    })
+    //> Finish Events Orang
+
+
+//> Start Events Semut
+const pSemut = document.querySelector(".semut");
+
+pSemut.addEventListener("click", function() {
+        const pilihanComputer = getPilihanComputer();
+        const pilihanPlayer = pSemut.className;
+        const hasil = getHasil(pilihanComputer, pilihanPlayer);
+
+        const imgComputer = document.querySelector(".img-computer");
+        imgComputer.setAttribute("src", "img/" + pilihanComputer + ".png");
+
+        const info = document.querySelector(".info");
+        info.innerHTML = hasil;
+
+    })
+    //> Finish Events Semut
+
+    */
